9f0f3c0d1cc91a504444b9a7ecf752e21ba9aa54
nf-PostQuantum <nf@post-quantum.com>
2018-06-15 18:10:23 +0100

    Hide .idea files, make bestFit tail recursive

--- src/main/scala/example/fits/BestFit.scala
+++ src/main/scala/example/fits/BestFit.scala
@@ -1,18 +1,19 @@
 package example.fits
 
+import scala.annotation.tailrec
+
 object BestFit {
+
   def calculate(weights: List[Int], binSize: Int): Int = {
     var bins = List[Int]()
     for (weight <- weights) {
-      var i = 0
       var minimumSize = binSize + 1
-      var bi = 0
+      val bi = tailRecBestFit(bins, weight, binSize, 0, minimumSize, 0)
 
-      for (i <- bins.indices) {
-        if (bins(i) >= weight && bins(i) - weight < minimumSize) {
-          bi = i
-          minimumSize = bins(i) - weight
-        }
+      if (bi < bins.size && bins(bi) >= weight && bins(bi) - weight < minimumSize) {
+        minimumSize = bins(bi) - weight
+      } else {
+        minimumSize = binSize + 1
       }
 
       if (minimumSize == binSize + 1) {
@@ -22,5 +23,15 @@ object BestFit {
       }
     }
     bins.size
+  }
+
+//  Init (minimumSize = binSize + 1)
+  @tailrec
+  def tailRecBestFit(bins: List[Int], weight: Int, binSize: Int, bi: Int, minimumSize: Int, counter: Int): Int = {
+    bins match {
+      case Nil => bi
+      case head :: tail if head >= weight && head - weight < minimumSize => tailRecBestFit(tail, weight, binSize, counter, head-weight, counter + 1)
+      case _ :: tail => tailRecBestFit(tail, weight, binSize, bi, minimumSize, counter + 1)
+    }
   }
 }
